4ea1ce3aea5da559e8c8019a4fae8bef9d1e6a3d
Merseong <esc990720@korea.ac.kr>
2019-09-29 19:02:50 +0900

    Done Cube Moving

---
+++ Being Cooked/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float sensitivity = 10;
+    public float underLimit = 0.1f;
+
+    public Transform parent;
+    public Transform target;
+
+    public bool isMoving = true;
+
+    private void Start()
+    {
+        if (isMoving)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    isMoving = false;
+        //    Cursor.visible = true;
+        //    Cursor.lockState = CursorLockMode.None;
+        //}
+
+        float mouseMoveX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseMoveY = Input.GetAxis("Mouse Y") * sensitivity;
+
+        transform.RotateAround(target.position, Vector3.up, mouseMoveX);
+        if (transform.position.y > underLimit || mouseMoveY < 0) transform.RotateAround(target.position, transform.right, -mouseMoveY);
+        if (transform.position.y <= underLimit) transform.RotateAround(target.position, transform.right, mouseMoveY);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 camToParent = parent.position - transform.position;
+        camToParent.y = 0;
+
+        parent.forward = camToParent;
+    }
+}
---
+++ Being Cooked/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    public float speed = 30;
+
+    public Transform parent;
+    public Transform director;
+
+    private bool isJumped = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Respawn")) isJumped = false;
+    }
+
+    private void Update()
+    {
+        Vector3 horizontal = director.right * Input.GetAxis("Horizontal") * speed;
+        Vector3 vertical = director.forward * Input.GetAxis("Vertical") * speed;
+
+        GetComponent<Rigidbody>().AddForce(horizontal + vertical);
+
+        if (!isJumped && Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * speed / 2, ForceMode.VelocityChange);
+            isJumped = true;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        parent.position = transform.position;
+    }
+}

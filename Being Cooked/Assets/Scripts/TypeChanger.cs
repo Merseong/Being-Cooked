7eadd26f2dde6b2758db77f91d264fa7c93cfb69
JeongWooSeok <www013181@gmail.com>
2019-10-06 20:22:48 +0900

    재료와 콜라이더 처리함

--- Being Cooked/Assets/Scripts/TypeChanger.cs
+++ Being Cooked/Assets/Scripts/TypeChanger.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class TypeChanger : MonoBehaviour
 {
+    public Transform camPos;
+    public Ingredient inIngredient;
+
+
+
     // Start is called before the first frame update
     void Start()
     {

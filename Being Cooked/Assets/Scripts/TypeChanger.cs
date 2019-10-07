c73e85a1db707d3d2c8328c461847128c847e7be
JeongWooSeok <www013181@gmail.com>
2019-10-07 14:03:47 +0900

    간단한 주석

--- Being Cooked/Assets/Scripts/TypeChanger.cs
+++ Being Cooked/Assets/Scripts/TypeChanger.cs
@@ -7,7 +7,7 @@ public class TypeChanger : MonoBehaviour
     public Transform camPos;
     public Ingredient inIngredient;
 
-
+    //colider에 들어오면 카메라 이동 후 고정, 재료 변환 && 이동
 
     // Start is called before the first frame update
     void Start()
